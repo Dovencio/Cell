7d6092ccbcad0675120ba8c676be4e4ac430b8cf
Dovencio <dovencio.doshorte@gmail.com>
2018-04-21 19:10:36 -0600

    Documentation
    Forgot to save

---
+++ EleList.h
@@ -0,0 +1,8 @@
+#ifndef ELELIST_H_INCLUDED
+#define ELELIST_H_INCLUDED
+
+#include "Include.h"
+
+
+
+#endif // ELELIST_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
 #include "Include.h"
+#include "EleList.h"
+
+/** \note Read Me
+ *
+ * To use notes, make a comment and use one of the following notations:
+ * Priority |Name
+ * =========|=======
+ * 1        | TODO
+ * 2        | @todo
+ * 3        | \todo
+ * =================
+ * 1        | FIXME
+ * 2        | @fixme
+ * 3        | \fixme
+ * =================
+ * 1        | NOTE
+ * 2        | @note
+ * 3        | \note
+ *
+ * To see a list of exiting notes
+ * View -> Todo list
+ *
+ */
+
+
 
 int main()
 {
-    cout << string test = "Hello world!" << endl;
+    /** Beginning of program
+     *
+     * Things like
+     * Startup,
+     * Variable calls,
+     * etc.
+     *
+     */
+
+    while
+
     return 0;
 }
